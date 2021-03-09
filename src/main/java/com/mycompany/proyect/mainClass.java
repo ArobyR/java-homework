d40443784bc47b7fa9d9af694bf1733003984c83
ArobyR <ar.rosario001@gmail.com>
2021-03-09 13:18:18 -0800

    add cuenta instance

--- src/main/java/com/mycompany/proyect/mainClass.java
+++ src/main/java/com/mycompany/proyect/mainClass.java
@@ -5,6 +5,8 @@
  */
 package com.mycompany.proyect;
 
+import java.util.Scanner;
+
 /**
  *
  * @author arros_000
@@ -15,13 +17,65 @@ public class mainClass {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        Persona persona1 = new Persona();
-        Persona persona2 = new Persona();
+        // TODO code application logic 
+        Scanner sc = new Scanner(System.in);
+        String nombre;
+        int edad, peso, altura;
+        char sexo;
+
+        System.out.println(
+                "Introduce el nombre: ");
+        nombre = sc.nextLine();
+
+        System.out.println(
+                "Introduce la edad: ");
+        edad = sc.nextInt();
+
+        System.out.println(
+                "Introduce el peso: ");
+        peso = sc.nextInt();
+
+        System.out.println(
+                "Introduce la altura: ");
+        altura = sc.nextInt();
+
+        System.out.println(
+                "Introduce el sexo, (M para masculino, F para femenino): ");
+        sexo = sc.next().charAt(0);
+
+        // nombre, edad, sexo    
+        // nombre, edad, peso, altura, sexo
+        Persona persona1 = new Persona(nombre, edad, peso, altura, sexo);
+        Persona persona2 = new Persona(nombre, edad, sexo);
         Persona persona3 = new Persona();
+
+        persona3.setNombre(nombre);
+
+        persona3.SetEdad(edad);
+
+        persona3.setAltura(altura);
+
+        persona3.setPeso(peso);
+
+        persona3.setSexo(sexo);
+
+        persona1.printEstadoDePeso();
+        persona2.printEstadoDePeso();
+        persona3.printEstadoDePeso();
+
+        persona1.printValidacionEdad();
+        persona2.printValidacionEdad();
+        persona3.printValidacionEdad();
+
+        System.out.println(persona1.toString());
+        System.out.println(persona2.toString());
+        System.out.println(persona3.toString());
         
-        // nombre, edad, sexo, peso y altura
-        // no peso y altura
-        // todo
+        cuenta cuenta1 = new cuenta("The Flyer");
+        cuenta cuenta2 = new cuenta("The Gigant", 20);
+        
+        System.out.println(cuenta1.toString());
+        System.out.println(cuenta2.toString());
     }
+
 }
